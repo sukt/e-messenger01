9cebbdd866dc7e1ea4aeab55ea8357283f014612
Kubota <suk4.atc@gmail.com>
2017-03-09 10:11:58 +0000

    carrierwaveの導入完了。

--- elites-messenger/app/controllers/application_controller.rb
+++ elites-messenger/app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+#deviseコントローラ実行時に以下のメソッドを実行
+  beforeaction :configure_permitted_parameters, if: :devise_contoroller?
+  
+  private
+  #新規作成の際、以下の３つのパラメータを含める必要がある
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :thmubnail, :agreement])
+  end
+
 end
---
+++ elites-messenger/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  #carrierWavenにて作成したUserThumbUploaderを使用する
+
+  mount_uploader :thumbnail, UserThumbnailUploader
+  
+end
