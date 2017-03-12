20a3dc26e4ef58456db976a12107b16dd5cf2ca8
Kubota <suk4.atc@gmail.com>
2017-03-12 07:48:52 +0000

    基本課題４まで。

--- elites-messenger/app/controllers/application_controller.rb
+++ elites-messenger/app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
 #deviseコントローラ実行時に以下のメソッドを実行
-  beforeaction :configure_permitted_parameters, if: :devise_contoroller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
   #新規作成の際、以下の３つのパラメータを含める必要がある
